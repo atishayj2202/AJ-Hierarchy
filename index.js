12598e56de4f660d6e6435dd9d731919b912c9a9
atishayj2202 <atishayj2202@gmail.com>
2019-12-15 19:44:20 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Import stylesheets
-import './style.css';
-
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var firebaseConfig = {
+  apiKey: "AIzaSyDyzC6Cwo86vIK47QNUCnj9uxXe8VboJYw",
+  authDomain: "aj-hierarchy.firebaseapp.com",
+  databaseURL: "https://aj-hierarchy.firebaseio.com",
+  projectId: "aj-hierarchy",
+  storageBucket: "aj-hierarchy.appspot.com",
+  messagingSenderId: "650588222359",
+  appId: "1:650588222359:web:372f580db0b9b931770ad2",
+  measurementId: "G-60C2SSEM05"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
