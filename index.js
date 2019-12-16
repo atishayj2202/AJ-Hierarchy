648ee7ee0a22a2006086b8990c3e1108d7e9fce4
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 13:59:14 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import "./style.css"
+
+function getin(){
+  firebase.auth().signInWithEmailAndPassword(document.getElementById("uname").Value, document.getElementById("pword").Value).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+}
