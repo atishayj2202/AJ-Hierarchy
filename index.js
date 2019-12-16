96f5e3529756626ad6faf3871cb6f38fa122e457
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 15:28:30 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import "./style.css"
 function showsup(){
   document.getElementById("signin").style.visibility = "hidden";
+  document.getElementById("signin").style.height = 0;
+  document.getElementById("signin").style.margin = 0;
+  document.getElementById("signin").style.overflow = "hidden";
   document.getElementById("signup").style.visibility = "visible";
 }
 function showsin(){
