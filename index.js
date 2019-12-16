865e5c760cb3929411c8743477b9f4c88c44f384
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 17:40:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function showsup(){
   document.getElementById("signin").style.padding = 0;
 
   document.getElementById("signup").style.visibility = "visible";
+
+
+  //signup
+  const signupform = document.querySelector('#sup');
+  signupform.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const email = signupform["uname-up"].value;
+    const password = signupform["pword-up"].value;
+    console.log(email);
+    console.log(password);
+    /*auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+      Console.log(cred);
+    })*/
+  });
 }
 function showsin(){
   document.getElementById("signup").style.visibility = "hidden";
@@ -17,3 +31,4 @@ function showsin(){
 }
 document.getElementById("bin").addEventListener("click", showsup);
 document.getElementById("bup").addEventListener("click", showsin);
+
