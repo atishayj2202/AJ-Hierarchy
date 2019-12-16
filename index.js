53866b4f2106acf86880b5414d05584e382c3614
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 15:50:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ function showsup(){
   document.getElementById("signin").style.height = 0;
   document.getElementById("signin").style.margin = 0;
   document.getElementById("signin").style.padding = 0;
+
   document.getElementById("signup").style.visibility = "visible";
 }
-function showsup(){
+function showsin(){
   document.getElementById("signup").style.visibility = "hidden";
   document.getElementById("signin").style.visibility = "visible";
   document.getElementById("signin").style.height = auto;
   document.getElementById("signin").style.padding = 10;
 }
-document.getElementById("bin").addEventListener = "showsup"
+document.getElementById("bin").addEventListener("click", showsup);
+document.getElementById("bup").addEventListener("click", showsin);
