2c60f5d7d06d53be9d4ec3faec309427e7e3e391
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 15:47:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import "./style.css"
 function showsup(){
-  //document.getElementById("signin").style.visibility = "hidden";
+  document.getElementById("signin").style.visibility = "hidden";
   document.getElementById("signin").style.height = 0;
-  document.getElementById("signin").style.width = 0;
   document.getElementById("signin").style.margin = 0;
-  document.getElementById("signin").style.overflow = "hidden";
   document.getElementById("signin").style.padding = 0;
-  
   document.getElementById("signup").style.visibility = "visible";
 }
-function showsin(){
+function showsup(){
   document.getElementById("signup").style.visibility = "hidden";
   document.getElementById("signin").style.visibility = "visible";
+  document.getElementById("signin").style.height = auto;
+  document.getElementById("signin").style.padding = 10;
 }
-document.getElementById("bin").addEventListener("click",showsup);
+document.getElementById("bin").addEventListener = "showsup"
