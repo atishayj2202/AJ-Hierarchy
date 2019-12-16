c0796a320ff7c4fa3ccbf68c553a372ddb4b7288
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 15:21:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function showsup(){
   document.getElementById("signin").style.visibility = "hidden";
   document.getElementById("signup").style.visibility = "visible";
 }
+function showsup(){
+  document.getElementById("signup").style.visibility = "hidden";
+  document.getElementById("signin").style.visibility = "visible";
+}
