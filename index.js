292f9f39cd9497a29b82022f8a369db6675050f2
atishayj2202 <atishayj2202@gmail.com>
2019-12-16 18:31:17 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import "./style.css"
 function showsup(){
-  document.getElementById("signin").style.visibility = "hidden";
-  document.getElementById("signin").style.height = 0;
-  document.getElementById("signin").style.margin = 0;
-  document.getElementById("signin").style.padding = 0;
-
-  document.getElementById("signup").style.visibility = "visible";
-
-
+  hidesignin();
+  showsignup();
   //signup
   const signupform = document.querySelector('#sup');
   signupform.addEventListener('submit', (e)=>{
@@ -21,17 +15,42 @@ function showsup(){
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
+      alert(errorMessage);
+    }).then(cred => {
+      console.log(cred);
+      hidesignup();
     });
   });
 }
-function showsin(){
-  document.getElementById("signup").style.visibility = "hidden";
+function showsignin(){
   document.getElementById("signin").style.visibility = "visible";
   document.getElementById("signin").style.height = "auto";
   document.getElementById("signin").style.padding = 10;
   document.getElementById("signin").style.margin = "auto";
-  
 }
+function showsignup(){
+  document.getElementById("signup").style.visibility = "visible";
+  document.getElementById("signup").style.height = "auto";
+  document.getElementById("signup").style.padding = 10;
+  document.getElementById("signup").style.margin = "auto";  
+}
+function hidesignup(){
+  document.getElementById("signup").style.visibility = "hidden";
+  document.getElementById("signup").style.height = 0;
+  document.getElementById("signup").style.padding = 0;
+  document.getElementById("signup").style.margin = 0;   
+}
+function hidesignin(){
+  document.getElementById("signin").style.visibility = "hidden";
+  document.getElementById("signin").style.height = 0;
+  document.getElementById("signin").style.padding = 0;
+  document.getElementById("signin").style.margin = 0;   
+}
+/*function showsin(){
+  showsignin();
+  hidesignup();
+  const signinform = document
+}*/
 document.getElementById("bin").addEventListener("click", showsup);
 document.getElementById("bup").addEventListener("click", showsin);
 
